b4dab75a5f1ff3f5fa8d5d3cc1ad2aa7ff006fd5
alfonsoGallardoGO <jose.gallardo@grupo-ortiz.com>
2025-08-01 17:37:28 -0600

    feat:cambios para el build

--- resources/js/Pages/Suppliers/Suppliers/Index.vue
+++ resources/js/Pages/Suppliers/Suppliers/Index.vue
@@ -4,8 +4,8 @@ import AppLayout from "@/Layouts/AppLayout.vue";
 import { ref, onMounted, computed } from "vue";
 import { FilterMatchMode } from "@primevue/core/api";
 import { useToast } from "primevue/usetoast";
-import { useForm } from "@inertiajs/inertia-vue3";
-import { router } from '@inertiajs/vue3';
+import { useForm } from "@inertiajs/vue3";
+import { router } from "@inertiajs/vue3";
 
 const props = defineProps({
     suppliers: Array,
@@ -43,7 +43,6 @@ const dt = ref();
 const supplierDialog = ref(false);
 const selectedBenefits = ref();
 
-
 const filters = ref({
     global: { value: null, matchMode: FilterMatchMode.CONTAINS },
 });
@@ -58,7 +57,6 @@ const hideDialog = () => {
 };
 
 const editSupplier = async (id) => {
-
     try {
         const updatedData = {
             name: supplierForm.name,
@@ -66,21 +64,25 @@ const editSupplier = async (id) => {
             phone: supplierForm.phone,
         };
 
-        const response = await axios.put(route('suppliers.update', id), updatedData);
+        const response = await axios.put(
+            route("suppliers.update", id),
+            updatedData
+        );
 
         hideDialog();
         toast.add({
             severity: "success",
             summary: "Actualizado",
-            detail: response.data.message || 'Proveedor actualizado con éxito.',
+            detail: response.data.message || "Proveedor actualizado con éxito.",
             life: 3000,
         });
         router.reload();
-
     } catch (error) {
         console.log(error);
         hideDialog();
-        const errorMessage = error.response?.data?.message || 'Error al actualizar el proveedor.';
+        const errorMessage =
+            error.response?.data?.message ||
+            "Error al actualizar el proveedor.";
         toast.add({
             severity: "error",
             summary: "Error",
@@ -88,7 +90,6 @@ const editSupplier = async (id) => {
             life: 3000,
         });
     }
-
 };
 
 const show = async (id) => {
@@ -107,7 +108,6 @@ const show = async (id) => {
     }
 };
 
-
 const showSelect = () => {
     if (!tipo.value || !tipo.value.value) {
         intervalo.value = null;
@@ -119,7 +119,10 @@ const showSelect = () => {
     <AppLayout title="Proveedores">
         <div class="wrapper d-flex flex-column flex-row-fluid" id="kt_wrapper">
             <Header :title="'Proveedores'" />
-            <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+            <div
+                class="content d-flex flex-column flex-column-fluid"
+                id="kt_content"
+            >
                 <div class="container-fluid" id="kt_content_container">
                     <div class="card">
                         <Toast />
@@ -144,71 +147,157 @@ const showSelect = () => {
                                 />
                             </template>
                         </Toolbar> -->
-                        <DataTable ref="dt" v-model:selection="selectedBenefits" :value="suppliers" dataKey="id"
-                            paginator :rows="10" :filters="filters" :rowsPerPageOptions="[5, 10, 25]"
-                            currentPageReportTemplate="Mostrando {first} a {last} de {totalRecords} proveedores">
+                        <DataTable
+                            ref="dt"
+                            v-model:selection="selectedBenefits"
+                            :value="suppliers"
+                            dataKey="id"
+                            paginator
+                            :rows="10"
+                            :filters="filters"
+                            :rowsPerPageOptions="[5, 10, 25]"
+                            currentPageReportTemplate="Mostrando {first} a {last} de {totalRecords} proveedores"
+                        >
                             <template #header>
-                                <div class="flex flex-wrap gap-2 items-center justify-between">
+                                <div
+                                    class="flex flex-wrap gap-2 items-center justify-between"
+                                >
                                     <h4 class="m-0">Catalogo De Proveedores</h4>
                                     <IconField>
                                         <InputIcon>
                                             <i class="pi pi-search" />
                                         </InputIcon>
-                                        <InputText v-model="filters['global'].value" placeholder="Buscar..." />
+                                        <InputText
+                                            v-model="filters['global'].value"
+                                            placeholder="Buscar..."
+                                        />
                                     </IconField>
                                 </div>
                             </template>
 
-                            <Column style="width: 3rem" :exportable="false"></Column>
-                            <Column field="id" header="Id" sortable style="min-width: 6rem"></Column>
-                            <Column header="Nombre" sortable style="min-width: 18rem">
+                            <Column
+                                style="width: 3rem"
+                                :exportable="false"
+                            ></Column>
+                            <Column
+                                field="id"
+                                header="Id"
+                                sortable
+                                style="min-width: 6rem"
+                            ></Column>
+                            <Column
+                                header="Nombre"
+                                sortable
+                                style="min-width: 18rem"
+                            >
                                 <template #body="slotProps">
-                                    {{ slotProps.data.legal_name_company || slotProps.data.name }}
+                                    {{
+                                        slotProps.data.legal_name_company ||
+                                        slotProps.data.name
+                                    }}
                                 </template>
                             </Column>
-                            <Column field="tax" header="RFC" sortable style="min-width: 10rem"></Column>
-                            <Column field="phone" header="Telefono" sortable style="min-width: 10rem"></Column>
-                            <Column field="email" header="Correo" sortable style="min-width: 12rem"></Column>
+                            <Column
+                                field="tax"
+                                header="RFC"
+                                sortable
+                                style="min-width: 10rem"
+                            ></Column>
+                            <Column
+                                field="phone"
+                                header="Telefono"
+                                sortable
+                                style="min-width: 10rem"
+                            ></Column>
+                            <Column
+                                field="email"
+                                header="Correo"
+                                sortable
+                                style="min-width: 12rem"
+                            ></Column>
 
                             <Column :exportable="false" style="min-width: 4rem">
                                 <template #body="slotProps">
-                                    <Button icon="pi pi-pencil" severity="help" outlined rounded class="mr-2"
-                                        @click="show(slotProps.data.id)" />
+                                    <Button
+                                        icon="pi pi-pencil"
+                                        severity="help"
+                                        outlined
+                                        rounded
+                                        class="mr-2"
+                                        @click="show(slotProps.data.id)"
+                                    />
                                 </template>
                             </Column>
                         </DataTable>
 
-                        <Dialog v-model:visible="supplierDialog" :style="{ width: '450px' }"
-                            header="Editar Campos Proveedor" :modal="true">
+                        <Dialog
+                            v-model:visible="supplierDialog"
+                            :style="{ width: '450px' }"
+                            header="Editar Campos Proveedor"
+                            :modal="true"
+                        >
                             <div class="flex flex-col gap-6 p-6">
                                 <div class="flex flex-col gap-6">
                                     <div class="flex flex-col hidden">
-                                        <label class="text-sm font-semibold text-gray-700 mb-1 flex items-center gap-2">
-                                            <i class="pi pi-user text-red-400"></i> Nombre
+                                        <label
+                                            class="text-sm font-semibold text-gray-700 mb-1 flex items-center gap-2"
+                                        >
+                                            <i
+                                                class="pi pi-user text-red-400"
+                                            ></i>
+                                            Nombre
                                         </label>
-                                        <InputText v-model="supplierForm.name" placeholder="xxxxxxxxxx"
-                                            class="w-full p-inputtext-sm" />
+                                        <InputText
+                                            v-model="supplierForm.name"
+                                            placeholder="xxxxxxxxxx"
+                                            class="w-full p-inputtext-sm"
+                                        />
                                     </div>
                                     <div class="flex flex-col">
-                                        <label class="text-sm font-semibold text-gray-700 mb-1 flex items-center gap-2">
-                                            <i class="pi pi-envelope text-red-400"></i> Correo
+                                        <label
+                                            class="text-sm font-semibold text-gray-700 mb-1 flex items-center gap-2"
+                                        >
+                                            <i
+                                                class="pi pi-envelope text-red-400"
+                                            ></i>
+                                            Correo
                                         </label>
-                                        <InputText v-model="supplierForm.email" placeholder="correo@ejemplo.com"
-                                            class="w-full p-inputtext-sm" />
+                                        <InputText
+                                            v-model="supplierForm.email"
+                                            placeholder="correo@ejemplo.com"
+                                            class="w-full p-inputtext-sm"
+                                        />
                                     </div>
                                     <div class="flex flex-col">
-                                        <label class="text-sm font-semibold text-gray-700 mb-1 flex items-center gap-2">
-                                            <i class="pi pi-phone text-teal-500"></i> Teléfono
+                                        <label
+                                            class="text-sm font-semibold text-gray-700 mb-1 flex items-center gap-2"
+                                        >
+                                            <i
+                                                class="pi pi-phone text-teal-500"
+                                            ></i>
+                                            Teléfono
                                         </label>
-                                        <InputText v-model="supplierForm.phone" placeholder="+52 000 000 0000"
-                                            class="w-full p-inputtext-sm" />
+                                        <InputText
+                                            v-model="supplierForm.phone"
+                                            placeholder="+52 000 000 0000"
+                                            class="w-full p-inputtext-sm"
+                                        />
                                     </div>
                                 </div>
                             </div>
 
                             <template #footer>
-                                <Button label="Cancel" icon="pi pi-times" text @click="hideDialog" />
-                                <Button label="Guardar" icon="pi pi-check" @click="editSupplier(supplierForm.id)" />
+                                <Button
+                                    label="Cancel"
+                                    icon="pi pi-times"
+                                    text
+                                    @click="hideDialog"
+                                />
+                                <Button
+                                    label="Guardar"
+                                    icon="pi pi-check"
+                                    @click="editSupplier(supplierForm.id)"
+                                />
                             </template>
                         </Dialog>
                     </div>
