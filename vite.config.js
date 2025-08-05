580ebd04128965355c27d08f4c0cd964c2547a95
alfonsoGallardoGO <jose.gallardo@grupo-ortiz.com>
2025-08-05 13:24:25 -0600

    merge branch Jorge

--- vite.config.js
+++ vite.config.js
@@ -19,9 +19,6 @@ export default defineConfig({
                 },
             },
         }),
-        Components({
-            resolvers: [PrimeVueResolver()],
-        }),
     ],
     resolve: {
         alias: {
