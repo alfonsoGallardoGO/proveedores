5480cfb197ad14841998f48c526c86f1c264eef8
abdielGraciano <luisabdiel.gv@gmail.com>
2025-08-14 11:16:58 -0600

    pdf

--- resources/js/Components/PdfViewer.vue
+++ resources/js/Components/PdfViewer.vue
@@ -6,7 +6,7 @@
 import { ref, onMounted, watch } from 'vue';
 import * as pdfjsLib from 'pdfjs-dist';
 // Código corregido para la importación
-import PdfjsWorker from '~/pdfjs-dist/build/pdf.worker.min.js?url';
+import PdfjsWorker from '~/pdfjs-dist/build/pdf.worker.min.mjs?url';
 
 const props = defineProps({
     pdfUrl: {
