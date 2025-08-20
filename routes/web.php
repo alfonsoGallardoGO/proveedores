2e46fc337dd95425e92d0382006cd89d5ec0f70e
alfonsoGallardoGO <jose.gallardo@grupo-ortiz.com>
2025-08-20 12:11:18 -0600

    fix:eliminar rutas y footer

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\DashboardController;
 Route::prefix('suppliers')->group(function () {
     Route::resource('purchase-orders', SupplierPurchaseOrderController::class);
     Route::resource('purchase-orders-invoices', SupplierInvoicesController::class);
-    Route::resource('supplier', SupplierController::class);
+    //Route::resource('supplier', SupplierController::class);
 });
 
 
@@ -38,11 +38,11 @@ Route::middleware([
     // Route::get('/dashboard', function () {return Inertia::render('Dashboard');})->name('dashboard');
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-    Route::get('/users', [SuplierUserController::class, 'index'])->name('users.index');
-    Route::post('/users', [SuplierUserController::class, 'store'])->name('users.store');
-    Route::delete('/users/{id}', [SuplierUserController::class, 'destroy'])->name('users.destroy');
-    Route::put('/users/{id}/update', [SuplierUserController::class, 'update'])->name('users.update');
-    Route::delete('/users', [SuplierUserController::class, 'destroySelected'])->name('users.destroySelected');
+    // Route::get('/users', [SuplierUserController::class, 'index'])->name('users.index');
+    // Route::post('/users', [SuplierUserController::class, 'store'])->name('users.store');
+    // Route::delete('/users/{id}', [SuplierUserController::class, 'destroy'])->name('users.destroy');
+    // Route::put('/users/{id}/update', [SuplierUserController::class, 'update'])->name('users.update');
+    // Route::delete('/users', [SuplierUserController::class, 'destroySelected'])->name('users.destroySelected');
 
     Route::resource('supplier-whatsapp', SupplierWhatsappController::class)->only(['index']);
 });
